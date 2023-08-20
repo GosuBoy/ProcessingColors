b3190ef6ccc7a403366005fd086a716b68f8cb26
Javier Chavez <122915269+GosuBoy@users.noreply.github.com>
2023-08-19 19:20:51 -0500

    Added a bare-bones options component (#4)

--- PColors/src/App.jsx
+++ PColors/src/App.jsx
@@ -1,11 +1,15 @@
 import { CPickerV3 } from "./components/colorPickerV3/CPickerV3";
+import { Options } from "./components/options/Options";
 
 function App() {
     return (
-        <div className="ml-56">
-            <h1 className="text-8xl font-bold">PCOLORS</h1>
-            <CPickerV3 />
-        </div>
+        <>
+            <h1 className="text-8xl font-bold">ProcessingColors </h1>
+            <div className="mx-[50px] flex">
+                <Options />
+                <CPickerV3 />
+            </div>
+        </>
     );
 }
 
