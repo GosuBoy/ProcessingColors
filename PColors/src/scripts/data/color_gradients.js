ea17e8b79e32bc38ec3846e5af6c4c31c4331aa1
Javier_Chavez <david.o66@hotmail.com>
2023-08-21 16:31:50 -0500

    Improved theme data organization

---
+++ PColors/src/scripts/data/color_gradients.js
@@ -0,0 +1,26 @@
+export const ui_color_data_gradients = [
+    {
+        id: "toolbar",
+        color_type: "gradient",
+        pcolorB: ["toolbar.gradient.bottom", "#242528"],
+        pcolorT: ["toolbar.gradient.top", "#242528"],
+    },
+    {
+        id: "header",
+        color_type: "gradient",
+        pcolorB: ["header.gradient.bottom", "#242528"],
+        pcolorT: ["header.gradient.top", "#242528"],
+    },
+    {
+        id: "editor",
+        color_type: "gradient",
+        pcolorB: ["editor.gradient.bottom", "#202124"],
+        pcolorT: ["editor.gradient.top", "#202124"],
+    },
+    {
+        id: "footer",
+        color_type: "gradient",
+        pcolorB: ["footer.gradient.bottom", "#42464E"],
+        pcolorT: ["footer.gradient.top", "#42464E"],
+    },
+];
---
+++ PColors/src/scripts/data/color_solid.js
@@ -0,0 +1,267 @@
+export const ui_color_data_solid = [
+    {
+        id: "toolbar_button_enabled_field",
+        color_type: "solid",
+        pcolor: ["toolbar.button.enabled.field", "#999EA6"],
+    },
+    {
+        id: "toolbar_button_enabled_glyph",
+        color_type: "solid",
+        pcolor: ["toolbar.button.enabled.glyph", "#0A233A"],
+    },
+    {
+        id: "toolbar_button_pressed_field",
+        color_type: "solid",
+        pcolor: ["toolbar.button.pressed.field", "#00152B"],
+    },
+    {
+        id: "toolbar_button_pressed_glyph",
+        color_type: "solid",
+        pcolor: ["toolbar.button.pressed.glyph", "#B7CEF3"],
+    },
+    {
+        id: "toolbar_button_pressed_stroke",
+        color_type: "solid",
+        pcolor: ["toolbar.button.pressed.stroke", "#B7CEF3"],
+    },
+    {
+        id: "toolbar_button_rollover_field",
+        color_type: "solid",
+        pcolor: ["toolbar.button.rollover.field", "#B7CEF3"],
+    },
+    {
+        id: "toolbar_button_rollover_glyph",
+        color_type: "solid",
+        pcolor: ["toolbar.button.rollover.glyph", "#0A233A"],
+    },
+    {
+        id: "toolbar_button_selected_field",
+        color_type: "solid",
+        pcolor: ["toolbar.button.selected.field", "#FFFFFE"],
+    },
+    {
+        id: "toolbar_button_selected_glyph",
+        color_type: "solid",
+        pcolor: ["toolbar.button.selected.glyph", "#464C58"],
+    },
+    {
+        id: "toolbar_rollover_color",
+        color_type: "solid",
+        pcolor: ["toolbar.rollover.color", "#FFFFFE"],
+    },
+    {
+        id: "header_tab_arrow_color",
+        color_type: "solid",
+        pcolor: ["header.tab.arrow.color", "#5E5F61"],
+    },
+    {
+        id: "header_tab_modified_color",
+        color_type: "solid",
+        pcolor: ["header.tab.modified.color", "#fbff00"],
+    },
+    {
+        id: "header_tab_selected_color",
+        color_type: "solid",
+        pcolor: ["header.tab.selected.color", "#090B0E"],
+    },
+    {
+        id: "header_tab_unselected_color",
+        color_type: "solid",
+        pcolor: ["header.tab.unselected.color", "#171819"],
+    },
+    {
+        id: "header_text_selected_color",
+        color_type: "solid",
+        pcolor: ["header.text.selected.color", "#A5B1C4"],
+    },
+    {
+        id: "header_text_unselected_color",
+        color_type: "solid",
+        pcolor: ["header.text.unselected.color", "#5E5F61"],
+    },
+    {
+        id: "editor_bgcolor",
+        color_type: "solid",
+        pcolor: ["editor.bgcolor", "#000000"],
+    },
+    {
+        id: "editor_bracket_highlight_color",
+        color_type: "solid",
+        pcolor: ["editor.bracket.highlight.color", "#006698"],
+    },
+    {
+        id: "editor_caret_color",
+        color_type: "solid",
+        pcolor: ["editor.caret.color", "#FFFFFE"],
+    },
+    {
+        id: "editor_column_error_color",
+        color_type: "solid",
+        pcolor: ["editor.column.error.color", "#FFFFFE"],
+    },
+    {
+        id: "editor_column_warning_color",
+        color_type: "solid",
+        pcolor: ["editor.column.warning.color", "#999EA6"],
+    },
+    {
+        id: "editor_completion_class_color",
+        color_type: "solid",
+        pcolor: ["editor.completion.class.color", "#F89300"],
+    },
+    {
+        id: "editor_completion_field_color",
+        color_type: "solid",
+        pcolor: ["editor.completion.field.color", "#1A2CF1"],
+    },
+    {
+        id: "editor_completion_local_color",
+        color_type: "solid",
+        pcolor: ["editor.completion.local.color", "#EB90AD"],
+    },
+    {
+        id: "editor_completion_method_color",
+        color_type: "solid",
+        pcolor: ["editor.completion.method.color", "#D4006B"],
+    },
+    {
+        id: "editor_eol_markers_color",
+        color_type: "solid",
+        pcolor: ["editor.eol_markers.color", "#999999"],
+    },
+    {
+        id: "editor_error_underline_color",
+        color_type: "solid",
+        pcolor: ["editor.error.underline.color", "#FFFFFE"],
+    },
+    {
+        id: "editor_fgcolor",
+        color_type: "solid",
+        pcolor: ["editor.fgcolor", "#FFFFFE"],
+    },
+    {
+        id: "editor_gutter_highlight_color",
+        color_type: "solid",
+        pcolor: ["editor.gutter.highlight.color", "#535558"],
+    },
+    {
+        id: "editor_gutter_text_color",
+        color_type: "solid",
+        pcolor: ["editor.gutter.text.color", "#FAFFFF"],
+    },
+    {
+        id: "editor_line_highlight_color",
+        color_type: "solid",
+        pcolor: ["editor.line.highlight.color", "#191B1E"],
+    },
+    {
+        id: "editor_scrollbar_color",
+        color_type: "solid",
+        pcolor: ["editor.scrollbar.color", "#4E535A"],
+    },
+    {
+        id: "editor_scrollbar_thumb_enabled_color",
+        color_type: "solid",
+        pcolor: ["editor.scrollbar.thumb.enabled.color", "#6C7078"],
+    },
+    {
+        id: "editor_scrollbar_thumb_pressed_color",
+        color_type: "solid",
+        pcolor: ["editor.scrollbar.thumb.pressed.color", "#60656D"],
+    },
+    {
+        id: "editor_scrollbar_thumb_rollover_color",
+        color_type: "solid",
+        pcolor: ["editor.scrollbar.thumb.rollover.color", "#60656D"],
+    },
+    {
+        id: "editor_selection_color",
+        color_type: "solid",
+        pcolor: ["editor.selection.color", "#2D2F34"],
+    },
+    {
+        id: "editor_warning_underline_color",
+        color_type: "solid",
+        pcolor: ["editor.warning.underline.color", "#999EA6"],
+    },
+    {
+        id: "console_color",
+        color_type: "solid",
+        pcolor: ["console.color", "#000000"],
+    },
+    {
+        id: "console_error_color",
+        color_type: "solid",
+        pcolor: ["console.error.color", "#470063"],
+    },
+    {
+        id: "console_output_color",
+        color_type: "solid",
+        pcolor: ["console.output.color", "#ff2989"],
+    },
+    {
+        id: "console_scrollbar_color",
+        color_type: "solid",
+        pcolor: ["console.scrollbar.color", "#525252"],
+    },
+    {
+        id: "console_scrollbar_thumb_enabled_color",
+        color_type: "solid",
+        pcolor: ["console.scrollbar.thumb.enabled.color", "#4A5059"],
+    },
+    {
+        id: "console_scrollbar_thumb_pressed_color",
+        color_type: "solid",
+        pcolor: ["console.scrollbar.thumb.pressed.color", "#4A5059"],
+    },
+    {
+        id: "console_scrollbar_thumb_rollover_color",
+        color_type: "solid",
+        pcolor: ["console.scrollbar.thumb.rollover.color", "#4A5059"],
+    },
+    {
+        id: "footer_icon_enabled_color",
+        color_type: "solid",
+        pcolor: ["footer.icon.enabled.color", "#ABB0B9"],
+    },
+    {
+        id: "footer_icon_selected_color",
+        color_type: "solid",
+        pcolor: ["footer.icon.selected.color", "#FFFFFE"],
+    },
+    {
+        id: "footer_tab_enabled_color",
+        color_type: "solid",
+        pcolor: ["footer.tab.enabled.color", "#42464E"],
+    },
+    {
+        id: "footer_tab_selected_color",
+        color_type: "solid",
+        pcolor: ["footer.tab.selected.color", "#6C7078"],
+    },
+    {
+        id: "footer_text_enabled_color",
+        color_type: "solid",
+        pcolor: ["footer.text.enabled.color", "#ABB0B9"],
+    },
+    {
+        id: "footer_text_selected_color",
+        color_type: "solid",
+        pcolor: ["footer.text.selected.color", "#FFFFFE"],
+    },
+    {
+        id: "footer_updates_indicator_field_color",
+        color_type: "solid",
+        pcolor: ["footer.updates.indicator.field.color", "#00000C"],
+    },
+    {
+        id: "footer_updates_indicator_text_color",
+        color_type: "solid",
+        pcolor: ["footer.updates.indicator.text.color", "#FAFFFF"],
+    },
+    {
+        id: "footer_updates_text_color",
+        color_type: "solid",
+        pcolor: ["footer.updates.text.color", "#FFFFFE"],
+    },
+];
