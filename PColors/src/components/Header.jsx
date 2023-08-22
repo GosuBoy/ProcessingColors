22af344173d42408123da2f09ce8ef6e64b5b48e
Javier_Chavez <david.o66@hotmail.com>
2023-08-22 15:24:54 -0500

    Fixing styles in processingUI component

--- PColors/src/components/Header.jsx
+++ PColors/src/components/Header.jsx
@@ -2,7 +2,10 @@ import LogoIcon from "../assets/LOGO.svg";
 
 export function Header() {
     return (
-        <header className="w-full h-36 bg-black mb-28 border-b-[0.5px] border-white flex content-center flex-wrap px-[52px]">
+        <header
+            className="w-full h-36 mb-28 border-b-[0.5px] border-white flex content-center flex-wrap px-[52px]"
+            style={{ backgroundColor: "#0B0B0B" }}
+        >
             <Logo />
             <Nav />
         </header>
